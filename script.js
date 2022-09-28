713748b3eb4f352e280e044b2f4b65eb46e83f33
deviant <mb.deviant@gmail.com>
2022-09-28 14:25:10 +0300

    add basic elements to operate function

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ function add(a, b) {
 function subtract(a, b) {
     return a - b;
 }
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
-function divide (a,b){
-    return parseFloat((a/b).toFixed(1));
+function divide(a, b) {
+    return parseFloat((a / b).toFixed(1));
 }
 
-function operate(a,operator,b){
-    a = number(a);
-    b = number(b);
-    if (operator == '+'){
-        return a+b;
+function operate(a, operator, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '÷':
+            if (a == 0) return b;
+            if (b == 0) return "Cannot divide by zero";
+            return divide(a, b);
+        default:
+            return "choose a valid operator"
     }
+
+
 }
