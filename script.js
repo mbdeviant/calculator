308f7a8c643ffae1b31826d16b6285316e3df168
deviant <mb.deviant@gmail.com>
2022-10-03 23:14:09 +0300

    add inputs object

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const display = document.getElementById("display");
 const btnUndo = document.getElementById("undo");
 const allButtons = document.querySelectorAll('button');
+const btnTest = document.getElementById("equals");
 allButtons.forEach(function (btnClick) {
     btnClick.addEventListener('click', () => {
         display.textContent += btnClick.value;
         if (display.textContent.length >= 10) {
             display.textContent = display.textContent.slice(0, 10);
         }
+       
     });
 });
 btnUndo.addEventListener('click', () => {
     display.textContent = display.textContent.substring(0, display.textContent.length - 1);
 });
+
+const inputs ={
+    displayValue: '0',
+}
+
+btnTest.addEventListener('click',()=> inputs.displayValue = "patates");
+
 
 function add(a, b) {
     return a + b;
